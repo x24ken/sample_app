e6c6f660f834b549c62351f7979de5b11aa77927
x24ke <x24ken@gmail.com>
2017-10-30 03:47:05 +0000

    第7章終わり

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #保存の成功
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to @user
     else
       render 'new'
     end

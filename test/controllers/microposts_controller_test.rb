030ba66a2772f9e8549a0b010515eefb202c0b8c
x24ke <x24ken@gmail.com>
2017-11-09 07:11:54 +0000

    プロフィール完成

---
+++ test/controllers/microposts_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class MicropostsControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @micropost = microposts(:orange)
+  end
+  
+  test "ログインしてない人はマイクロポストを作ることはできない" do
+    assert_no_difference 'Micropost.count' do
+      post microposts_path, params: { micropost: { content: "Lorem ipsum" } }
+    end
+    assert_redirected_to login_url
+  end
+  
+  test "ログインしてない人はマイクロポストを消すことはできない" do
+    assert_no_difference 'Micropost.count' do
+      delete micropost_path(@micropost)
+    end
+  assert_redirected_to login_url
+end
+end

1e74434586abf4fc85c846b728fb32f2844f3fee
x24ke <x24ken@gmail.com>
2017-11-09 01:35:31 +0000

    micropostのバリテーション

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class MicropostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @user = users(:michael)
+    #このコードは慣習的に正しくない
+    @micropost = Micropost.new(content: "Lorem ipsum", user_id: @user.id)
+  end
+  
+  test "正常なモデルの有効性(valid)" do
+    assert @micropost.valid?
+  end
+  
+  test "user_idは存在するべき" do
+    @micropost.user_id = nil
+    assert_not @micropost.valid?
+  end
+  
+  test "contentは存在するべき" do
+    @micropost.content = "   "
+    assert_not @micropost.valid?
+  end
+  
+  test "contentは140文字まで" do
+    @micropost.content = "a" * 141
+    assert_not @micropost.valid?
+  end
 end

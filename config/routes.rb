74a5bb5a2a921257bb4dd5cc9d2829c405eedc4c
x24ke <x24ken@gmail.com>
2017-11-16 08:38:32 +0000

    フォローボタン作成

--- config/routes.rb
+++ config/routes.rb
@@ -21,11 +21,17 @@ Rails.application.routes.draw do
 
   get 'password_resets/edit'
   
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   
   resources :account_activations, only: [:edit]
   
   resources :password_resets,     only: [:new, :create, :edit, :update]
   
   resources :microposts,          only: [:create, :destroy]
+  
+  resources :relationships,       only: [:create, :destroy]
 end
